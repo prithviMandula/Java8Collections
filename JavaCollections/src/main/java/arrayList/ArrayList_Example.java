22dbf74d3c2d4962a0962b3d8100520887fcc085
prithvi <mandulaprithvi@gmail.com>
2017-01-24 08:31:38 +0530

    Adds ArrayList Example and Pom file
    Adds Array list example in ArrayList package
    
    Change-Id: I102968a2f3f900d9b7bcdd9e7edaa8ff717fdeee
    Signed-off-by: prithvi <mandulaprithvi@gmail.com>

---
+++ JavaCollections/src/main/java/arrayList/ArrayList_Example.java
@@ -0,0 +1,41 @@
+package arrayList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+/**
+ * Class explaining the usage of ArrayList
+ * 
+ * @author prithviMandula
+ */
+public class ArrayList_Example {
+
+    public static void main(String[] args) {
+        ArrayList<String> data = new ArrayList<String>();
+
+        data.add("Adding");
+        data.add("Elements");
+        data.add("to");
+        data.add("ArrayList");
+
+        // Using an Iterator
+        System.out.println("Printing Array List using Iterator");
+        Iterator<String> itr = data.iterator();
+        while(itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+
+        // Using advanced for loop
+        System.out.println("Printing Array List using For loop");
+        for(String word : data) {
+            System.out.println(word);
+        }
+
+        // Using Java 8 Lambda Expression
+        System.out.println("Printing Array List using Java 8 Lambda Expressions");
+        data.forEach((word)-> {
+            System.out.println(word);
+        });
+    }
+}
